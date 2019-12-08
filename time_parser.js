c44a7de970ee3fee17117915e9b87c46cb8c95d5
Namrita Murali <namrita_murali.apple.com>
2019-12-07 21:16:02 -0800

    Corrected time parsing logic

--- time_parser.js
+++ time_parser.js
@@ -1,43 +1,55 @@
 // regex for time parsing
+
+function insert(str, index, value) {
+    return str.substr(0, index) + value + str.substr(index);
+}
+
 function timeParser ( messageBody )
 {
-	var time_re = "^(?:([0-9]*):)?([0-9]*)$";
+	var time_re = "^([0-9]*):([0-9]*)$";
 	var result = messageBody.match( time_re );
-	var colon_pos =  3;
+	var str_len = messageBody.length;
+	var colon_pos =  str_len - 2;
 	var total = 0;
 
 	if ( !messageBody )
 	{
 		console.log( "Message body has no contents\n" );
+		return total;
 	}
 	if ( !result )
 	{
-		console.log( "No colon present in input\n" );
-		insert( messageBody, colon_pos, ':' );
-		console.log( "Updated body:  " + messageBody );
-		timeParser( messageBody );
+		messageBody = insert( messageBody, colon_pos, ':' );
+		return timeParser( messageBody );
 	}
 	else
 	{
 		var result_len = result.length;
 		var seconds = result[result_len - 1];
+		console.log( "seconds: " + seconds );
 		if ( !seconds )
 		{
 			console.log( "Bad input\n" );
 			return total;
 		}
-		// seconds = int( seconds );
+		seconds = parseInt( seconds );
 		if ( seconds > 60 )
 		{
 			console.log( "Bad input\n" );
 			return total;
 		}
-		var minutes = result[0];
-		if ( !minutes ) total = seconds;
-		// minutes = int( minutes );
-		total = minutes * 60 +  seconds;
+		var minutes = result[result_len - 2];
+		if ( !minutes ) 
+		{
+			total = seconds;
+		}
+		else 
+		{
+			minutes = parseInt( minutes );
+			total = minutes * 60 + seconds;
+		}
 	}
-return total;
+	return total;
 }
 
 function timeToString( seconds )
